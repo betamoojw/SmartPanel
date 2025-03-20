c9c030412a6079f1fc2158aeabe3751874c41c9d
betamoojw <40590760+betamoojw@users.noreply.github.com>
2025-03-20 23:22:34 +0800

    Added more coffee type both in SLS and PIO.

--- SLS/Coffee wending_events.py
+++ SLS/Coffee wending_events.py
@@ -15,22 +15,18 @@ def button_irish(event_struct):
     return
 
 
+def button_Macchiato(event_struct):
+    return
+
+
+def button_cream(event_struct):
+    return
+
+
 def Payment_screen_loaded(event_struct):
     return
 
 
-def Prep_Screen_Loaded(event_struct):
-    return
-
-
 def Prep_screen_loaded(event_struct):
-    return
-
-
-def switch_screen(event_struct):
-    return
-
-
-def switch_Scree(event_struct):
     return
 
